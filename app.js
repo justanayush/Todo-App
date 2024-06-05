ab7528db22575f4c1fab1479a3a46fee1ff548c6
VishAyush <139580532+INFERNO1211@users.noreply.github.com>
2024-06-05 12:40:10 +0530

    done with todo app learned new things : )

--- app.js
+++ app.js
@@ -2,15 +2,74 @@ const input = document.querySelector('#input-text');
 const btn = document.querySelector('.add-btn');
 const todos = document.querySelector('.todos')
 
-btn.addEventListener('click', () => {
-    if (input.value === ''){
-        alert('Please enter a task.')
+input.addEventListener('keypress', (e)=>{
+    console.log(e.key)
+    if (e.key === 'Enter'){
+        task();
+        input.value = "";
     }
-    else{
-        let li = document.createElement('li');
-        let val = input.value;
-        li.innerHTML = val
-        console.log(val)
-        todos.appendChild(li)
+});
+
+function task(){
+    let li = document.createElement('li');
+            let p = document.createElement('p');
+            let img1 = document.createElement('img');
+            img1.setAttribute('src','images/1.jpg');
+            img1.setAttribute('class', 'unchecked');
+            p.appendChild(img1);
+            let img2 = document.createElement('img');
+            img2.setAttribute('src','images/cross.png');
+            img2.setAttribute('class', 'delete');
+            let span = document.createElement('span');
+            let val = input.value;
+            span.innerHTML = val;
+            p.appendChild(span);
+            p.appendChild(img2);
+            li.appendChild(p);
+            todos.appendChild(li);
+            saveData();
+}
+function addTask(){
+    btn.addEventListener('click', (e) => {
+        if (input.value === ''){
+            alert('Please enter a task.')
+        }
+        else{
+            task();
+            }
+        input.value = ""
+    })
+
+}
+todos.addEventListener('click',(e)=>{
+    function toggleClass(){
+        if(e.target.parentElement.querySelector('.unchecked').getAttribute('src') === "images/1.jpg"){
+            e.target.parentElement.querySelector('.unchecked').setAttribute('src', 'images/2.jpg')
+        }else{
+            e.target.parentElement.querySelector('.unchecked').setAttribute('src', 'images/1.jpg')
+        }
+        saveData();
+    }
+    if(e.target.tagName === "SPAN"){
+        e.target.classList.toggle('task-complete');
+        toggleClass();
+       
+    }
+    if(e.target.tagName === "IMG" && e.target.classList.contains('unchecked')){
+        e.target.parentElement.querySelector('span').classList.toggle('task-complete');
+        toggleClass();
+    }
+    if(e.target.tagName === "IMG" && e.target.classList.contains('delete')){
+        e.target.parentElement.remove();
+        saveData();
     }
 })
+addTask();
+
+function saveData(){
+    localStorage.setItem('data', todos.innerHTML);
+}
+function showData(){
+    todos.innerHTML = localStorage.getItem('data');
+}
+showData();
